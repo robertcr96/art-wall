5a3cecf151619c3aa28a20dfc2ee7224823b7802
Robert <robertcr96@gmail.com>
2017-10-30 18:20:32 +0200

    add react

---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+ Volume in drive E has no label.
+ Volume Serial Number is C655-BD29
+
+ Directory of E:\NodeJs Apps\art-wall\src
+
+10/30/2017  06:19 PM    <DIR>          .
+10/30/2017  06:19 PM    <DIR>          ..
+10/30/2017  06:19 PM                 0 Counter.js
+10/30/2017  06:18 PM               293 main.js
+               2 File(s)            293 bytes
+               2 Dir(s)  63,380,905,984 bytes free
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
- Volume in drive E has no label.
- Volume Serial Number is C655-BD29
+console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
 
- Directory of E:\NodeJs Apps\art-wall\src
-
-10/30/2017  05:31 PM    <DIR>          .
-10/30/2017  05:31 PM    <DIR>          ..
-10/30/2017  05:31 PM                 0 main.js
-               1 File(s)              0 bytes
-               2 Dir(s)  63,391,227,904 bytes free
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
